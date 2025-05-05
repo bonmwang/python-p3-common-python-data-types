81c30c0d6795b6f97f8ef9a9f1e5e2bac22eab25
Boniface <bm715452@gmail.com>
2025-05-06 02:23:05 +0300

    data types

---
+++ lib/app.py
@@ -0,0 +1,96 @@
+"I'm a string"
+'Me too!'
+
+str("I'm a string")
+
+dog_name = "Lucy"
+print(f"Say hello to my do {dog_name}")
+
+price_1 = 3
+print(f"Item 1 costs ${price_1:.2f}")
+
+price_2 = 2.5
+print(f"Item 2 costs ${price_2:.2f}")
+
+"hello"
+
+"hello".upper()
+
+"HELLO".lower()
+
+"hello".capitalize()
+
+"hello" + "world"
+
+"hello" * 3
+
+type("hello")
+
+dir("hello")
+
+int("1")
+
+int(1.1)
+
+float("1.1")
+
+4 / 3
+4/3.0
+float(4 / 3)
+
+[1, 3, 400, 7]
+list()
+
+list_abc = ['a', 'b', 'c']
+list_abc[0]
+list_abc[1]
+list_abc[2]
+
+len([1, 3, 400, 7])
+
+sorted([5, 100, 234, 7, 2])
+
+list_123 = [1, 2, 3]
+list_123.pop()
+list_123.remove(1)
+print(list_123)
+
+(1, 2, 3)
+tuple([1, 2, 3])
+
+set()
+set(3, 2, 3, 'a', 'b', 'a')
+set([3, 2, 3, 'a', 'b', 'a'])
+
+s = {1, 2, 3}
+s.pop()
+s.remove(3)
+
+{ "key1": "value1", "key2": "value2" }
+
+my_dict = { "key1": 1, "key2": 2 }
+my_dict["key2"]
+
+my_dict = { "key1": "value1", "key2": "value2"}
+print(my_dict["key3"])
+print(my_dict.get("key3"))
+
+my_dict = { "key1": "value1", "key2": "value2"}
+my_dict.key2
+
+dict(x = 1, y = 2)
+
+no_value = None
+print(no_value)
+
+type(True)
+type(False)
+
+not True
+not False
+not 1
+not 0
+not ''
+not []
+not ()
+not {}
